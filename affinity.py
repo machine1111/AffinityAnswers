ae961088e427c7ca4817b952863dc8a3cc2dd18b
machine1111 <dwakandan@gmail.com>
2022-05-18 18:06:10 +0530

    adding files to git

---
+++ affinity.py
@@ -0,0 +1,24 @@
+import re
+
+try:
+    with open('test_tweets.txt', 'r') as f:
+        text = f.read()
+    sentences = text.split('\n')
+finally:
+    f.close()
+
+profane_words = ['bad','worse','worst','fat','ugly','stupid']
+
+for sentence in sentences:
+    print(sentence)
+    # Removing all punchuation from sentence
+    sentence = re.sub(r'[^\w\s]','',sentence)
+    if len(sentence) > 0:
+        sentence = sentence.split(' ')
+        count = 0
+        # Comparing each word of the sentence to set of words provided
+        if any(prof.lower() == word.lower() for word in sentence for prof in profane_words):
+            count += 1
+
+        profanity = round(count/len(sentence) * 100, 2)
+        print(f'Profanity : {profanity}%')
